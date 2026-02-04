d320fbc8bed25473fba63503492f67ba211a497a
Abzntfound <adube6113@outlook.com>
2026-02-04 19:44:21 +0000

    Update auth.js

--- auth.js
+++ auth.js
@@ -2,7 +2,7 @@
 // auth.js
 
 // YOUR GOOGLE SHEETS WEB APP URL (Replace this after deploying Google Apps Script)
-const GOOGLE_SHEETS_URL = 'https://script.google.com/macros/s/AKfycbxL9uGhGx2wNblYTypg5FFu2wKbuNWrzeqAkWeC8b2zZu6JGVDyBjNNiV0ljrl3mZlTlA/exec';
+const GOOGLE_SHEETS_URL = 'https://script.google.com/macros/s/AKfycbx527wq0vGwyr2mQ7mGy7LGGny7IamcZB6EOzA2aLeXG_3LW2vBoBXIF3fWX6x-z0QOTA/exec';
 
 // Check if user is already logged in
 window.addEventListener('DOMContentLoaded', () => {
